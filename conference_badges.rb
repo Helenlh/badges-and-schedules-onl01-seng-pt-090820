0b6a0ad0c06b5fd4f6918876d48692a54d15b5da
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-16 21:48:00 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,50 @@
-# Write your code here.
+ badge = "Hello, my name is #{name}."
+    let(:name) {"Arel"}
+  let(:attendees) {["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]}
+  let(:badges) {[
+                  "Hello, my name is Edsger.",
+                  "Hello, my name is Ada.",
+                  "Hello, my name is Charles.",
+                  "Hello, my name is Alan.",
+                  "Hello, my name is Grace.",
+                  "Hello, my name is Linus.",
+                  "Hello, my name is Matz."
+  end 
+  # it 'should return a formatted badge' do
+  #     expect(badge_maker(name)).to eq("Hello, my name is #{name}.")
+
+
+
+def badge_maker(name)
+  sample = []
+  name.each do | persons|
+  sample << persons
+  
+  end
+end 
+
+
+def conference_badges[]
+ 
+end
+ 
+ 
+  def badge_badge_creator(sample)
+   
+ end 
+ 
+ 
+  
+def badge_maker(attendees)
+    badge = []
+     attendees.each do |person|
+      puts "Hello, my name is #{person}"
+      badge << (person)
+      end
+    end 
+  
+  
+  def assign_rooms(attendees, rooms)
+    
+  end
+end 
