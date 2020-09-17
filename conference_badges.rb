2b53e317ebd03170b1af0d57fd04f4f6aaa6335f
HELEN HARMAN <hlharman@bellsouth.net>
2020-09-17 03:32:11 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,21 +1,25 @@
 def conference_badges (name)
-         "Hello, my name is #{name}"
+end
+
+  def badge_maker(name)    
+      puts "HEllo, my name is #{name}"
+   end 
+
+
+  def batch_badge_creator(attendees)
+    badges = []
+    attendees.each do |persons|
+    badges << "Hello, mhy name is #{attendees}, "
+   end
   end 
   
-
-  def badge_maker(attendees)
-    badge = []
-    badge.each do |persons|
-    badge << attendees 
+  def assign_rooms(rooms, attendees)
   end 
 
 
-  def batch_badge_creator()
-end 
 
-  def assign_rooms(rooms, attendees)
-end 
-
-  def conference_badge_printer
-end
+  def conference_badge_printer(attendees)
+  attendees.each do |persons|
+  puts "Welcome #{attendees}, you are assigned to room #{room}+1."
+  end
 end
